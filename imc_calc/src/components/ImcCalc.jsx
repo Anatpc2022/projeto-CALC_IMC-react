2791e37a43dc65db8ff06f8c11047ef6c76a2f6d
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-15 23:24:47 -0300

    Função de limpar formulário

--- imc_calc/src/components/ImcCalc.jsx
+++ imc_calc/src/components/ImcCalc.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react"
 import Button from "./Button"
 import "./ImcCalc.css"
 
 const ImcCalc = () => {
+
+    const [heigth, setHeigth] = useState("")
+    const [weigth, setWeigth] = useState("")
+
+    const clearForm = (e) => {
+        e.preventDefault()
+        setHeigth("")
+        setWeigth("")
+    }
+
+
     return (
         <div id="calc-container">
             <h2>Calculadora de IMC</h2>
@@ -13,7 +25,9 @@ const ImcCalc = () => {
                         type="text" 
                         name="height" 
                         id="height" 
-                        placeholder="Exemplo 1,58" 
+                        placeholder="Exemplo 1,58"
+                        onChange={(e) => setHeigth(e.target.value)}
+                        value={heigth} 
                         />
                     </div>
                     <div className="form-control">
@@ -22,13 +36,15 @@ const ImcCalc = () => {
                         type="text" 
                         name="weight" 
                         id="weight" 
-                        placeholder="Exemplo 69,5" 
+                        placeholder="Exemplo 69,5"
+                        onChange={(e) => setWeigth(e.target.value)}
+                        value={weigth} 
                         />
                     </div>
                 </div>
                 <div className="action-control">
                    <Button id="calc-btn" text="Calcular" /> 
-                   <Button id="clear-btn" text="Limpar" />  
+                   <Button id="clear-btn" text="Limpar" action={clearForm}/>  
                 </div>
             </form>
         </div>
